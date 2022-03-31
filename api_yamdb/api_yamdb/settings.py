538c22b2cff4b8a6314a110aad824b7165c290dc
Dmitry <kolosovdv@rambler.ru>
2022-03-31 09:34:56 +0300

    100

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'), 
 
-        'HOST': os.getenv('DB_HOST', default='127.0.0.1'), 
+        'HOST': os.getenv('DB_HOST', default='db'), 
 
         'PORT': os.getenv('DB_PORT', default='5432') 
 
